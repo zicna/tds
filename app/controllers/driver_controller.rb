cf1e56099334eb1f3f56b0789d87175135b9867a
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-03-29 00:19:47 -0500

    driver signin route (get/post), edit drivers/signup and show pages.

--- app/controllers/driver_controller.rb
+++ app/controllers/driver_controller.rb
@@ -6,8 +6,15 @@ class DriverController < ApplicationController
 
     #driver signin page
     post '/drivers' do
-        binding.pry
-        erb :""
+        #binding.pry
+        @driver = Driver.create(params[:driver])
+        redirect to "/drivers/#{@driver.id}"
     end
+
     #driver show page
+    get '/drivers/:id' do
+       # binding.pry
+        @diver = Driver.find_by(id: params[:id])
+        erb :"drivers/show"
+    end
 end
